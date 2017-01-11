8a252c6ac165857720f03881780c9b97d146a730
zamarawka <somebody.told@mail.ru>
2017-01-11 18:35:28 +0300

    Prepare to package

--- src/vue/index.js
+++ src/vue/index.js
@@ -2,12 +2,9 @@
 
 import uiKit from './uikit'
 import VueEvents from './vue-events'
-import VueResource from 'vue-resource'
 import Validator from 'validatorjs';
 
 if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(VueEvents)
-    window.Vue.use(VueResource)
     window.Vue.use(uiKit)
 
     window.uiKit = uiKit
--- src/vue/uikit.js
+++ src/vue/uikit.js
@@ -1,54 +1,8 @@
-import uiSelect     from './components/ui-select';
-import uiButton     from './components/ui-button';
-import uiToggle     from './components/ui-toggle';
-import uiInput      from './components/ui-input';
-import uiInputAjax  from './components/ui-input-ajax';
-import uiInputSpin  from './components/ui-input-spinner';
-import uiTabs       from './components/ui-tabs';
-import uiTab        from './components/ui-tab';
-import uiDropdown   from './components/ui-dropdown';
-import uiSpinner    from './components/ui-spinner';
-import uiPopup      from './components/ui-popup';
-import uiBoxFilter  from './components/ui-box-filter';
-import uiDatepicker  from './components/ui-datepicker';
-import uiProgressBar from './components/ui-progress-bar';
-import uiInkRipple  from './components/ui-ink-ripple/ui-Ink-ripple';
-import uiChip       from './components/ui-chip';
-import uiGroup      from './components/ui-group';
-import uiCollapsible from './components/collapsible/collapsible';
-import uiCollapsibleItem from './components/collapsible/collapsible-item';
-import uiRange       from './components/ui-range';
-import uiPagination  from './components/ui-pagination';
-import uiCheckbox  from './components/ui-checkbox';
-import uiLabel     from './components/ui-label';
+import VueResource from 'vue-resource'
 
+import VueEvents from './vue-events'
+import components from './components';
 import untilDeclOfNum from './until/declOfNum';
-
-export const components = {
-    'ui-select': uiSelect,
-    'ui-button': uiButton,
-    'ui-toggle': uiToggle,
-    'ui-input': uiInput,
-    'ui-input-ajax': uiInputAjax,
-    'ui-input-spinner': uiInputSpin,
-    'ui-tabs': uiTabs,
-    'ui-tab': uiTab,
-    'ui-spinner': uiSpinner,
-    'ui-box-filter': uiBoxFilter,
-    'ui-popup': uiPopup,
-    'ui-dropdown': uiDropdown,
-    'ui-datepicker': uiDatepicker,
-    'ui-progress-bar': uiProgressBar,
-    'ui-ink-ripple': uiInkRipple,
-    'ui-chip': uiChip,
-    'ui-group': uiGroup,
-    'ui-collapsible': uiCollapsible,
-    'ui-collapsible-item': uiCollapsibleItem,
-    'ui-range': uiRange,
-    'ui-pagination': uiPagination,
-    'ui-checkbox': uiCheckbox,
-    'ui-label': uiLabel
-}
 
 function helpers () {
     let untils = {
@@ -62,10 +16,14 @@ function helpers () {
     }
 }
 
+export * from './components';
+
 export default function plugin(Vue) {
     if (plugin.installed) return;
 
     helpers();
+    Vue.use(VueResource);
+    Vue.use(VueEvents);
 
     Object.keys(components).forEach(name => {
         Vue.component(name, components[name]);
