606e2cb96b3135ee1f71926672bc2e3150928ac5
zamarawka <somebody.told@mail.ru>
2017-01-12 10:20:18 +0300

    Clean global object.

--- src/vue/index.js
+++ src/vue/index.js
@@ -1,15 +1,9 @@
 "use strict";
 
 import uiKit from './uikit'
-import VueEvents from './vue-events'
-import Validator from 'validatorjs';
 
 if (typeof window !== 'undefined' && window.Vue) {
     window.Vue.use(uiKit)
-
-    window.uiKit = uiKit
-    window.VueEvents = VueEvents
-    window.Validator = Validator
 }
 
 export default uiKit
