bb0bec410cf86614ca28490a7ab0b6c2bb9d9e6b
zb3k <d.bourim@gmail.com>
2017-01-17 16:47:01 +0300

    add: ui-radio component

---
+++ components/ui-radio.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="form-group">
+        <input :id="id" :name="name" :checked="isChecked" @change="update" :disabled="disabled" class="ui-radio" type="radio">
+        <label :for="id" class="text">
+            <slot></slot>
+        </label>
+    </div>
+</template>
+
+<script>
+    export default{
+         props: {
+            value: {
+                type:    String,
+                default: ''
+            },
+            valueData: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            color: {
+                type: String
+            },
+            disabled: {
+                type: Boolean
+            }
+        },
+
+        data: () => ({}),
+
+        methods: {
+            update (e) {
+                this.$emit('input',  this.valueData)
+                this.$emit('change', this.valueData)
+            }
+        },
+
+        computed: {
+            id () {
+                return '_ui_radio_' + Math.random().toString(36).substr(2, 5);
+            },
+            isChecked () {
+                return this.value == this.valueData
+            },
+        },
+
+        mounted () {
+
+        }
+    }
+</script>
