1e046c2ed6ebae61de24f1e14b19b9552276fad2
faforty <faforty@ya.ru>
2017-01-18 10:31:42 +0300

    fix collapsible

--- components/collapsible/collapsible-item.vue
+++ components/collapsible/collapsible-item.vue
@@ -1,110 +1,104 @@
 <template>
-    <div :class="{ 'ui-accordion__item': true, 'ui-accordion__item--active': show }">
-        <h4 class="ui-accordion__item__head" @click="toggle">
+<div :class="{ 'ui-accordion__item': true, 'ui-accordion__item--active': show }">
+    <h4 class="ui-accordion__item__head" @click="toggle">
             <slot name="header"></slot>
             <span :class="['down', { up: show }]"><i class="uikit-chevron-down"></i></span>
         </h4>
-        <div ref="body" class="ui-accordion__item__content">
-            <slot name="body"></slot>
-        </div>
+    <div ref="body" class="ui-accordion__item__content">
+        <slot name="body"></slot>
     </div>
+</div>
 </template>
 
 <script>
-    var Velocity = require('velocity-animate')
+var Velocity = require('velocity-animate')
 
-    export default {
-        events: {
-            'collapsible-item::open': function (id, expendable) {
-                this.open(id, expendable)
-            },
-            'collapsible-item::close': function (id) {
-                return this.close(id)
+export default {
+    data() {
+        return {
+            show: false
+        }
+    },
+    computed: {
+        id() {
+            return this._uid
+        }
+    },
+    methods: {
+        openThis(immediately = false) {
+            if (!this.active) {
+                this.active = true;
+                if (immediately) {
+                    this.show = false
+                } else {
+                    this.show = true
+                    this.$nextTick(() => {
+                        Velocity(this.$refs.body, 'slideDown', this._slideConfig());
+                    });
+                }
             }
         },
-        data () {
-            return {
-                show: false
-            }
-        },
-        computed: {
-            id () {
-                return this._uid
-            }
-        },
-        methods: {
-            openThis(immediately = false) {
-                if (!this.active) {
-                    this.active = true;
-                    if (immediately) {
-                        this.show = false
-                    }
-                    else {
-                        this.show = true
-                        this.$nextTick(() => {
-                            Velocity(this.$refs.body, 'slideDown', this._slideConfig());
-                        });
-                    }
-                }
-            },
-            open(id, expendable) {
-                if (id === null || typeof id == "undefined") {
-                    this.openThis(true); // bulk open
-                }
-                else {
-                    if (id == this.id) {
-                        this.openThis();
-                    }
-                    else {
-                        if (!expendable) {
-                            this.closeThis();
-                        }
-                    }
-                }
-            },
-            closeThis(immediately = false) {
-                if (this.active) {
-                    this.active = false;
-                    if (immediately) {
-                        this.$els.body.style.height = '';
-                    }
-                    else {
-                        this.show = false
-                        this.$nextTick(() => {
-                           // this.$els.body.style.display = 'block';
-                            Velocity(this.$refs.body, 'slideUp', this._slideConfig());
-                        })
-                    }
-                }
-            },
-            close(id) {
-                if (id === null || typeof id == "undefined") {
-                    this.closeThis(true); // bulk close
-                }
-                else {
-                    if (id == this.id) {
+        open(id, expendable) {
+            if (id === null || typeof id == "undefined") {
+                this.openThis(true); // bulk open
+            } else {
+                if (id == this.id) {
+                    this.openThis();
+                } else {
+                    if (!expendable) {
                         this.closeThis();
                     }
                 }
-            },
-            toggle() {
-                if (this.active) {
-                    this.$events.$emit('collapsible::close', this.id);
+            }
+        },
+        closeThis(immediately = false) {
+            if (this.active) {
+                this.active = false;
+                if (immediately) {
+                    this.$els.body.style.height = '';
+                } else {
+                    this.show = false
+                    this.$nextTick(() => {
+                        // this.$els.body.style.display = 'block';
+                        Velocity(this.$refs.body, 'slideUp', this._slideConfig());
+                    })
                 }
-                else {
-                    this.$events.$emit('collapsible::open', this.id);
+            }
+        },
+        close(id) {
+            if (id === null || typeof id == "undefined") {
+                this.closeThis(true); // bulk close
+            } else {
+                if (id == this.id) {
+                    this.closeThis();
                 }
-            },
-            _slideConfig () {
-                return {
-                    duration: 350,
-                    easing: "easeOutQuart",
-                    queue: false,
-                    complete: () => {
-                        this.$refs.body.style.height = '';
-                    }
+            }
+        },
+        toggle() {
+            if (this.active) {
+                this.$parent.close(this.id);
+            } else {
+                this.$parent.open(this.id);
+            }
+        },
+        _slideConfig() {
+            return {
+                duration: 350,
+                easing: "easeOutQuart",
+                queue: false,
+                complete: () => {
+                    this.$refs.body.style.height = '';
                 }
             }
         }
+    },
+    mounted() {
+        this.$root.$on('collapsible-item::open', (id, expendable) => {
+            this.open(id, expendable)
+        })
+        this.$root.$on('collapsible-item::close', id => {
+            return this.close(id)
+        })
     }
+}
 </script>
