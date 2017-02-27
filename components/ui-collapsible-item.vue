9cd762a89b90f5cd610593c47d27ae52ce0dfd98
zb3k <d.bourim@gmail.com>
2017-02-27 14:57:45 +0300

    Fix+refactoring ui-collapsible ui-collapsible-item

---
+++ components/ui-collapsible-item.vue
@@ -0,0 +1,72 @@
+<template>
+<div :class="{'ui-accordion__item': true, 'ui-accordion__item--active': show }">
+    <h4 class="ui-accordion__item__head" @click="toggleContent">
+        <slot name="header"></slot>
+        <span :class="show ? 'down up' : 'down'"><i class="uikit-chevron-down"></i></span>
+    </h4>
+    <div ref="body" class="ui-accordion__item__content">
+        <slot name="body"></slot>
+    </div>
+</div>
+</template>
+
+<script>
+var Velocity = require('velocity-animate');
+
+export default {
+    props: {
+        opened: {
+            type: Boolean,
+            default: false,
+        }
+    },
+
+    data: () => ({
+        show: false
+    }),
+
+    computed: {
+        id() {
+            return this._uid
+        }
+    },
+
+    methods: {
+        toggleContent() {
+            this.show ? this.hideContent() : this.showContent();
+        },
+
+        showContent() {
+            this._lock = true;
+            this.$parent.$emit('collapsible-item::show');
+            this._lock = false;
+            this.show  = true;
+            Velocity(this.$refs.body, 'slideDown', this.velocityConfig());
+        },
+
+        hideContent() {
+            if (this._lock) {
+                return;
+            }
+            this.show = false;
+            Velocity(this.$refs.body, 'slideUp', this.velocityConfig());
+        },
+
+        velocityConfig() {
+            return {
+                duration: 350,
+                easing:   'easeOutQuart',
+                queue:    false,
+            }
+        }
+    },
+
+    mounted() {
+        if (this.opened) {
+            this.showContent();
+        }
+
+        this.$parent.$on('collapsible-item::show', this.hideContent);
+    }
+}
+</script>
