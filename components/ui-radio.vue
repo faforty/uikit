bd33d17918bf1551022d0e604fb523119c1f4298
zamarawka <somebody.told@mail.ru>
2017-03-28 15:26:40 +0300

    Fix import error

--- components/ui-radio.vue
+++ components/ui-radio.vue
@@ -19,43 +19,48 @@
 </template>
 
 <script>
-    export default{
-         props: {
-            value: {
-                type:    [Number, String],
-                default: ''
-            },
-            valueData: {
-                type:    [Number, String],
-                default: true
-            },
-            name: {
-                type: String
-            },
-            color: {
-                type: String
-            },
-            disabled: {
-                type: Boolean
-            },
-            hint: String,
-            info: String,
-        },
+import uiPopover from './components/ui-popover.vue';
 
-        methods: {
-            update (e) {
-                this.$emit('input',  this.valueData)
-                this.$emit('change', this.valueData)
-            }
+export default {
+    components: {
+        uiPopover
+    },
+     props: {
+        value: {
+            type:    [Number, String],
+            default: ''
         },
+        valueData: {
+            type:    [Number, String],
+            default: true
+        },
+        name: {
+            type: String
+        },
+        color: {
+            type: String
+        },
+        disabled: {
+            type: Boolean
+        },
+        hint: String,
+        info: String,
+    },
 
-        computed: {
-            id () {
-                return '_ui_radio_' + Math.random().toString(36).substr(2, 5);
-            },
-            isChecked () {
-                return this.value == this.valueData
-            },
+    methods: {
+        update (e) {
+            this.$emit('input',  this.valueData)
+            this.$emit('change', this.valueData)
         }
+    },
+
+    computed: {
+        id () {
+            return '_ui_radio_' + Math.random().toString(36).substr(2, 5);
+        },
+        isChecked () {
+            return this.value == this.valueData
+        },
     }
+}
 </script>
